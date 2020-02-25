5ef39e4f32e534419f68799f1307746d8936aafb
Cheng Gang <chenggangpro@gmail.com>
2020-02-25 22:00:14 +0800

    add remain files

---
+++ src/main/java/pro/chenggang/project/simpleapidoc/SimpleApiDocApplication.java
@@ -0,0 +1,13 @@
+package pro.chenggang.project.simpleapidoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleApiDocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleApiDocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/chenggang/project/simpleapidoc/SimpleApiDocApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.chenggang.project.simpleapidoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleApiDocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
